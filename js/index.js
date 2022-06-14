7c23a23736988099cbb4e5a3528a542b578b7a50
Alex <>
2022-06-14 21:31:51 +0300

    v.3

--- js/index.js
+++ js/index.js
@@ -1,49 +1,11 @@
-const imgs = document.getElementById('imgs')
-
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
 
 const choice = document.getElementById('choice')
 const num_active = document.getElementsByClassName('number_active')
 const num_paginat = document.getElementsByClassName('pagination_num')
 const dot = document.querySelectorAll("pagination_dots dot")
 
-const img = document.querySelectorAll('#imgs img')
-
 const upBtn = document.querySelector(".scrollBtn");
 
-
-// let ind = 1
-
-// choice.innerText = `${ind}/${img.length}`
-
-// rightBtn.addEventListener('click', () => {
-//   ind++
-//   changeImg()
-//   reserInterval()
-// })
-
-// leftBtn.addEventListener('click', () => {
-//   ind--
-//   changeImg()
-//   reserInterval()
-// })
-
-// function changeImg() {
-//   if (ind > img.length) {
-//     ind = 1
-//   } else if (ind < 1) {
-//     ind = img.length
-//   }
-//   imgs.style.transform = `translateX(${-ind * 500}px)`
-//   choice.innerHTML = `${ind}/${img.length}`
-  
-// }
-
-// function run() {
-//   ind++
-//   changeImg()
-// }
 
 // SCROLL================================================
 
@@ -85,19 +47,20 @@ burger.addEventListener('click', () => {
 
 // CHANGE SLIDER==========================================
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
 
 // ORDER WINDOW============================================
 
@@ -124,9 +87,45 @@ closeWindow.addEventListener('click', () => {
 })
 
 // =========================================
+
+const imgs = document.getElementById('imgs')
+
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+const img = document.querySelectorAll('#imgs img')
+
+let ind = 0
+
 // let interval = setInterval(run,4000)
 
+rightBtn.addEventListener('click', () => {
+    ind++
+    changeImg()
+    reserInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    ind--
+    changeImg()
+    reserInterval()
+})
+
+function changeImg() {
+    if (ind > img.length - 1){
+        ind = 0
+    } else if (ind < 0){
+        ind = img.length - 1
+    }
+    imgs.style.transform = `translateX(${-ind * 100}%)`
+}
+
 // function reserInterval(){
-  //     clearInterval(interval)
-  //     interval = setInterval(run,4000)
-  // }
+//     clearInterval(interval)
+//     interval = setInterval(run,4000)
+// }
+
+function run() {
+    ind++
+    changeImg()
+}
